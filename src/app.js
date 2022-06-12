071f0277a705072fff05b6d0de9e4268951a26cb
anruoxun <1916301475@qq.com>
2022-06-12 17:37:52 +0800

    安装chai

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import Button from "./components/button.vue"
+import Icon from "./components/icon.vue"
+import ButtonGroup from "./components/button-group.vue"
+import Switch from "./components/switch.vue"
 
 Vue.component('l-button', Button)
+Vue.component('l-icon', Icon)
+Vue.component('l-button-group', ButtonGroup)
+Vue.component('l-switch', Switch)
 new Vue({
   el: '#app',
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    test(){
+      console.log('click test')
+    }
+  }
 })
