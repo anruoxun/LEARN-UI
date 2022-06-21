d0f8ace5262c0979f15898635e17c390ab8c6ee0
anruoxun <1916301475@qq.com>
2022-06-21 11:11:53 +0800

    发布npm

---
+++ index.js
@@ -0,0 +1,17 @@
+import Button from "./components/button.vue"
+import Icon from "./components/icon.vue"
+import ButtonGroup from "./components/button-group.vue"
+import Switch from "./components/switch.vue"
+import Input from "./components/input.vue"
+import Row from "./components/Row.vue"
+import Col from "./components/Col.vue"
+
+export {
+  Button,
+  Icon,
+  ButtonGroup,
+  Switch,
+  Input,
+  Row,
+  Col,
+}
